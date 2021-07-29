93e685154819786a43e43bea4d6dd80857e17ef0
Svenden123 <buhrus1@yandex.ru>
2021-07-29 19:44:33 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+greeting = "Hello"
+first_name = "Jack"
+last_name = "White"
+exclamation_sign = "!"
+white_space = " "
+print(greeting + white_space + first_name + " " + last_name + exclamation_sign)
+long_string = 'This is long long string'
+print(long_string)
